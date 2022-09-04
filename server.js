9acb8e51a30308648b6f0a94089b227b97a6aa50
Kush Shah <kush@unc.edu>
2022-09-04 00:23:02 -0400

    change how minimist is instantiated

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const http = require('http')
 // Require fs module
 const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist');
+const argv = require('minimist')(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = argv.port || 3000;
@@ -15,7 +14,7 @@ const port = argv.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile('./public/index.html', 'Utf8', (err, data) => {
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
